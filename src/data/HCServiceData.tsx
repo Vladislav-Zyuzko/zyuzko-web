ca5cedb408b8983c9eb03d824c4a679a63cc7354
Vladislav Zyuzko <zyuzko142@gmail.com>
2024-01-06 19:47:02 +0600

    separate data for services

---
+++ src/data/HCServiceData.tsx
@@ -0,0 +1,42 @@
+import { appStrings } from "../strings";
+import searchImg from '../assets/images/services/search.png';
+import bottleImg from '../assets/images/services/bottle.png';
+import consultationImg from '../assets/images/services/consultation.png';
+import detailsInfoImg from '../assets/images/services/details_info.png';
+import pharmacyImg from '../assets/images/services/pharmacy.png';
+import trackingImg from '../assets/images/services/tracking.png';
+
+const services: HCServiceProps[] = [
+    {
+        title: appStrings.serviceTitle1,
+        description: appStrings.serviceDescription1,
+        imageUrl: searchImg,
+    },
+    {
+        title: appStrings.serviceTitle2,
+        description: appStrings.serviceDescription2,
+        imageUrl: bottleImg,
+    },
+    {
+        title: appStrings.serviceTitle3,
+        description: appStrings.serviceDescription3,
+        imageUrl: consultationImg,
+    },
+    {
+        title: appStrings.serviceTitle4,
+        description: appStrings.serviceDescription4,
+        imageUrl: detailsInfoImg,
+    },
+    {
+        title: appStrings.serviceTitle5,
+        description: appStrings.serviceDescription5,
+        imageUrl: pharmacyImg,
+    },
+    {
+        title: appStrings.serviceTitle6,
+        description: appStrings.serviceDescription6,
+        imageUrl: trackingImg,
+    },
+]
+
+export default services;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,7 @@ import HCTitle from '../../components/HCTitle/HCTitle';
 import HCArticle from '../../components/HCArticle/HCArticle';
 import Footer from '../../components/footer/Footer';
 import HomeCss from './home.module.css';
+import services from '../../data/HCServiceData';
 
 function Home() {
 
@@ -29,12 +30,7 @@ function Home() {
             {appStrings.servicesDescription}
           </div>
           <section id={HomeCss.servicesContainer}>
-            <HCService title={appStrings.serviceTitle1} description={appStrings.serviceDescription1} imageUrl='src/assets/images/services/search.png'/>
-            <HCService title={appStrings.serviceTitle2} description={appStrings.serviceDescription2} imageUrl='src/assets/images/services/bottle.png'/>
-            <HCService title={appStrings.serviceTitle3} description={appStrings.serviceDescription3} imageUrl='src/assets/images/services/consultation.png'/>
-            <HCService title={appStrings.serviceTitle4} description={appStrings.serviceDescription4} imageUrl='src/assets/images/services/details_info.png'/>
-            <HCService title={appStrings.serviceTitle5} description={appStrings.serviceDescription5} imageUrl='src/assets/images/services/pharmacy.png'/>
-            <HCService title={appStrings.serviceTitle6} description={appStrings.serviceDescription6} imageUrl='src/assets/images/services/tracking.png'/>
+            {services.map((item) => HCService(item))}
           </section>
           <div id={HomeCss.servicesButtonContainer}>
             <HCButton title={appStrings.servicesButton} isOutlined={true} />
