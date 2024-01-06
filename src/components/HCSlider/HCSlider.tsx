4a9a628496db94cb8d2cb77135ced5d55f056dab
Vladislav Zyuzko <zyuzko142@gmail.com>
2024-01-07 03:15:41 +0600

    release slider

--- src/components/HCSlider/HCSlider.tsx
+++ src/components/HCSlider/HCSlider.tsx
@@ -1,18 +1,48 @@
+import { useState } from 'react';
 import sliderContents from '../../data/HCSliderContentData';
 import HCTitle from '../HCTitle/HCTitle';
 import HCSliderContent from '../HCSliderContent/HCSliderContent';
 import HCSliderCss from './HCslider.module.css';
+import arrow from '../../assets/images/icons/arrow.svg';
 
 function HCSlider(props: HCTitleProps) {
+    const [currentIndex, changeCurrentIndex] = useState(0);
+  
+    const leftArrowHandleClick = () => {
+      changeCurrentIndex(currentIndex - 1);
+    };
+
+    const rightArrowHandleClick = () => {
+        changeCurrentIndex(currentIndex + 1);
+    };
+
     return <article className={HCSliderCss.sliderContainer}>
         <section className={HCSliderCss.sliderContent}>
             <HCTitle title={props.title} color={props.color} />
             <div className={HCSliderCss.sliderContentContainer}>
-                {HCSliderContent(sliderContents[0])}
+                {HCSliderContent(sliderContents[currentIndex])}
             </div>
         </section>
         <section className={HCSliderCss.sliderController}>
-            
+            <button 
+                className={HCSliderCss.sliderArrowButton} 
+                onClick={currentIndex != 0 ? leftArrowHandleClick : undefined}
+                style={currentIndex != 0 ? {opacity: 1.0,  cursor: 'pointer'} : {opacity: 0.4}}
+            >
+                <img src={arrow} alt="left-arrow" className={HCSliderCss.sliderArrow} id={HCSliderCss.leftSliderArrow}/>
+            </button>
+            <div className={HCSliderCss.sliderPoints}>
+                {sliderContents.map((_, index) => 
+                    <div className={HCSliderCss.sliderPoint} style={currentIndex == index ? {opacity: 1.0} : {opacity: 0.4}}></div>
+                )}
+            </div>
+            <button 
+                className={HCSliderCss.sliderArrowButton}
+                onClick={currentIndex != sliderContents.length - 1 ? rightArrowHandleClick : undefined}
+                style={currentIndex != sliderContents.length - 1 ? {opacity: 1.0,  cursor: 'pointer'} : {opacity: 0.4}}
+            >
+                <img src={arrow} alt="right-arrow" className={HCSliderCss.sliderArrow} />
+            </button>
         </section>
     </article>
 }
--- src/data/HCSliderContentData.tsx
+++ src/data/HCSliderContentData.tsx
@@ -1,13 +1,34 @@
 import { appStrings} from "../strings";
-import avatarImage1 from '../assets/images/avatar.png';
+import avatarImage1 from '../assets/images/avatars/avatar1.png';
+import avatarImage2 from '../assets/images/avatars/avatar2.png';
+import avatarImage3 from '../assets/images/avatars/avatar3.png';
+import avatarImage4 from '../assets/images/avatars/avatar4.png';
 
 const sliderContents: HCSliderContentProps[] = [
     {
-        avatarTitle: appStrings.sliderAvatarTitle,
-        avatarSubtitle: appStrings.sliderAvaterDescription,
+        avatarTitle: appStrings.sliderAvatarTitle1,
+        avatarSubtitle: appStrings.sliderAvatarDescription1,
         avatarImage: avatarImage1,
-        contentText: appStrings.sliderText,
-    }
+        contentText: appStrings.sliderText1,
+    },
+    {
+        avatarTitle: appStrings.sliderAvatarTitle2,
+        avatarSubtitle: appStrings.sliderAvatarDescription2,
+        avatarImage: avatarImage2,
+        contentText: appStrings.sliderText2,
+    },
+    {
+        avatarTitle: appStrings.sliderAvatarTitle3,
+        avatarSubtitle: appStrings.sliderAvatarDescription3,
+        avatarImage: avatarImage3,
+        contentText: appStrings.sliderText3,
+    },
+    {
+        avatarTitle: appStrings.sliderAvatarTitle4,
+        avatarSubtitle: appStrings.sliderAvatarDescription4,
+        avatarImage: avatarImage4,
+        contentText: appStrings.sliderText4,
+    },
 ]
 
 export default sliderContents;
